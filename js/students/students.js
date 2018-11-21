774c09088b7c8b38bc960867a087979cbfa36b76
fuasbi <gavin32174@gmail.com>
2018-11-21 12:50:13 -0500

    as

--- js/students/students.js
+++ js/students/students.js
@@ -278,7 +278,7 @@ async function updateStudent() {
 function gotoLaserQueue() {
 	var student_id = document.getElementById("student_profile");
 	student_id = student_id.getAttribute("student_id");
-	document.location.href = "http://104.248.113.22/html/students/students.html#student_id=" + student_id;
+	document.location.href = "http://104.248.113.22/html/lab/Laser_Cutter.html#student_id=" + student_id;
 }
 
 
