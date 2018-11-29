0d44225f7f7ce72c64eaf9e1ca4383ac7139abd8
MelvinXRamos <melvinxramos@knights.ucf.edu>
2018-11-29 00:07:20 -0500

    dfghj

--- js/management/machines.js
+++ js/management/machines.js
@@ -128,7 +128,7 @@ function editMachine(machine_id) {
             document.getElementById("restrictions_edit").setAttribute("placholder", machines[idx].restrictions);
         }
     }
-    console.log(obj.machines);
+    console.log(obj.machines[0][0]);
     
 	checkSwap("edit_Maching_form", "edit_Maching_form");
 	checkSwap("machine_list_block", "edit_Maching_form");
