238e3f5761b4edf3e95f90664f1d75f33a69bd8e
surajsingireddy <surajsingireddy@gmail.com>
2018-12-06 09:50:10 -0500

    sql changes
    made some changes

--- sql/Example_data.sql
+++ sql/Example_data.sql
@@ -14,6 +14,7 @@ INSERT INTO Major VALUES ("Industrial Engineering");
 INSERT INTO Student VALUES (null, "buraj_card_id", "buraj", "bingireddy", "Computer Science", "burajbingireddy@knights.ucf.edu", 0.0, 0.0, null);
 INSERT INTO Student VALUES (null, "", "N/A", "", "", "N/A", 0.0, 0.0, null);
 INSERT INTO Student VALUES (null, "a", "a", "a", "Computer Science", "a@ucf.edu", 0.0, 0.0, null);
+INSERT INTO Student VALUES (null, "1234567", "John", "Doe", "Mathematics", "johndoe@knights.ucf.edu", 0.0, 0.0, null);
 
 INSERT INTO Student VALUES (null, "1234567", "Mary", "Smith", "Computer Science", "mary.smith@ucf.edu", 0.0, 0.0, null);
 INSERT INTO Student VALUES (null, "1234568", "John", "Diaz", "Computer Science", "john.diaz@knights.ucf.edu", 0.0, 0.0, null);
@@ -249,11 +250,11 @@ INSERT INTO 3DPrint_Queue VALUES (null, 1, 4, 1, CURDATE(), NOW(), "01:30", "Wai
 INSERT INTO 3DPrint_Queue VALUES (null, 1, 5, 1, CURDATE(), NOW(), "03:00", "Waiting", "part_name", "path/to/file.txt", 1.0, 1.0, "coolstuff", "coolstuff2", "P O O P B O I S");
 
 
-INSERT INTO Laser_Queue VALUES (null, 1, 1, 1, CURDATE(), NOW(), "01:20:00", "Waiting");
+INSERT INTO Laser_Queue VALUES (null, 1, 4, 1, CURDATE(), NOW(), "01:20:00", "Waiting");
 INSERT INTO Laser_Queue VALUES (null, 1, 2, 1, CURDATE(), NOW(), "00:30:05", "Waiting");
-INSERT INTO Laser_Queue VALUES (null, 1, 1, 1, CURDATE(), NOW(), "00:40:00", "Waiting");
-INSERT INTO Laser_Queue VALUES (null, 1, 3, 1, CURDATE(), NOW(), "01:20:00", "Waiting");
-INSERT INTO Laser_Queue VALUES (null, 1, 3, 2, CURDATE(), NOW(), "00:30:05", "Waiting");
+INSERT INTO Laser_Queue VALUES (null, 1, 5, 1, CURDATE(), NOW(), "00:40:00", "Waiting");
+INSERT INTO Laser_Queue VALUES (null, 1, 8, 1, CURDATE(), NOW(), "01:20:00", "Waiting");
+INSERT INTO Laser_Queue VALUES (null, 1, 6, 2, CURDATE(), NOW(), "00:30:05", "Waiting");
 INSERT INTO Laser_Queue VALUES (null, 1, 5, 1, CURDATE(), NOW(), "00:40:00", "Waiting");
 INSERT INTO Laser_Queue VALUES (null, 1, 6, 3, CURDATE(), NOW(), "01:20:00", "Waiting");
 INSERT INTO Laser_Queue VALUES (null, 1, 7, 3, CURDATE(), NOW(), "00:30:05", "Waiting");
