ccf7a0a508c45970e5fc9a6b032e7e59c0235823
surajsingireddy <surajsingireddy@gmail.com>
2018-12-05 20:48:56 -0500

    api changes
    bugs

--- api/mobile/laserqueue/read.php
+++ api/mobile/laserqueue/read.php
@@ -12,7 +12,7 @@ $db = $database->getConnection();
 
 $data = json_decode(file_get_contents("php://input"));
 
-$query = "SELECT SEC_TO_TIME(SUM(SEC_TO_TIME(estimated_time))) AS overall_wait_time FROM Laser_Queue";
+$query = "SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(estimated_time))) AS overall_wait_time FROM Laser_Queue";
 $stmt = $db->prepare($query);
 
 if (!$stmt->execute()) {
@@ -24,7 +24,7 @@ if (!$stmt->execute()) {
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 extract($row);
 
-$query = "SELECT queue_pos,machine_id,Student.student_id,tech_id,date_added,estimated_time,time_added,status,first_name,(SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(estimated_time))) AS personal_wait_time FROM (Laser_Queue as lq) WHERE lq.queue_pos < Laser_Queue.queue_pos) FROM Laser_Queue JOIN Student ON Laser_Queue.student_id=Student.student_id WHERE date_added=CURDATE()";
+$query = "SELECT queue_pos,machine_id,Student.student_id,tech_id,date_added,estimated_time,time_added,status,first_name,(SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(estimated_time))) FROM (Laser_Queue as lq) WHERE lq.queue_pos < Laser_Queue.queue_pos) AS personal_wait_time FROM Laser_Queue JOIN Student ON Laser_Queue.student_id=Student.student_id WHERE date_added=CURDATE()";
 $stmt = $db->prepare($query);
 
 if (!$stmt->execute()) {
